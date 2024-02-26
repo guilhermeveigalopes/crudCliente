18134cf8984a749eefcf12c2cb67358ec3fc9b1d
guilhermeveigalopes <guilherme.vwiga@gmail.com>
2024-02-26 17:07:15 -0300

    Adicionar arquivos de projeto.

---
+++ crudCliente/Models/BancoContext.cs
@@ -0,0 +1,17 @@
+﻿using crudCliente.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudClientes.Models
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+    }
+
+}
---
+++ crudCliente/Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace crudCliente.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+
+    }
+}
